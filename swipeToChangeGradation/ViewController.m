138b0b0db21440aa227fa9c731ffe07f0c3c2fa3
Naoya Watanabe <hello@showandtell.jp>
2014-02-26 19:45:54 +0900

    commit

--- swipeToChangeGradation/ViewController.m
+++ swipeToChangeGradation/ViewController.m
@@ -18,12 +18,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //背景色をグラデーションに
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.view.bounds;
+    gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor greenColor] CGColor], (id)[[UIColor whiteColor] CGColor], nil];
+    [self.view.layer insertSublayer:gradient atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    CGPoint pos = [[touches anyObject] locationInView:self.view];
+    NSLog(@"pos(%.1f)", pos.y);
+    self.view.alpha = pos.y / 568;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    CGPoint pos = [[touches anyObject] locationInView:self.view];
+    NSLog(@"pos(%.1f)", pos.y);
+    self.view.alpha = pos.y / 568;
 }
 
 @end
